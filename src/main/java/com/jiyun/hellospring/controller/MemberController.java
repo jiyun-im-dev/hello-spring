c3d7a4e555e94cbb4ac3ee824c4be3031e32878a
jiyun-im-dev <jiyun.im.dev@gmail.com>
2024-12-19 12:00:55 +0900

    컴포넌트 스캔 방식 실습

---
+++ src/main/java/com/jiyun/hellospring/controller/MemberController.java
@@ -0,0 +1,17 @@
+package com.jiyun.hellospring.controller;
+
+import com.jiyun.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+}
--- src/main/java/com/jiyun/hellospring/service/MemberService.java
+++ src/main/java/com/jiyun/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package com.jiyun.hellospring.service;
 import com.jiyun.hellospring.domain.Member;
 import com.jiyun.hellospring.repository.MemberRepository;
 import com.jiyun.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
