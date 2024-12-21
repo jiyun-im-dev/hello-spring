e51abfca4b73b71bfe0bba541d012bf8b0601bf5
jiyun-im-dev <jiyun.im.dev@gmail.com>
2024-12-21 13:43:12 +0900

    스프링 DB 접근 기술: 스프링 데이터 JPA

--- src/main/java/com/jiyun/hellospring/SpringConfig.java
+++ src/main/java/com/jiyun/hellospring/SpringConfig.java
@@ -5,6 +5,7 @@ import com.jiyun.hellospring.repository.MemberRepository;
 import com.jiyun.hellospring.repository.MemoryMemberRepository;
 import com.jiyun.hellospring.service.MemberService;
 import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,23 +14,16 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 
 }
