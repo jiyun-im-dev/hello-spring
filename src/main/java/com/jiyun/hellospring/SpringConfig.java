519c641b236853efb7009b9586a241fc7889ac9e
jiyun-im-dev <jiyun.im.dev@gmail.com>
2024-12-21 15:23:39 +0900

    메서드 소요 시간 측정 AOP 적용

--- src/main/java/com/jiyun/hellospring/SpringConfig.java
+++ src/main/java/com/jiyun/hellospring/SpringConfig.java
@@ -1,15 +1,10 @@
 package com.jiyun.hellospring;
 
-import com.jiyun.hellospring.repository.JpaMemberRepository;
 import com.jiyun.hellospring.repository.MemberRepository;
-import com.jiyun.hellospring.repository.MemoryMemberRepository;
 import com.jiyun.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
